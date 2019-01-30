40a456baadf908b2dbde47ece158490c2185b3e7
Daniel Sim <daniel.ssq89@gmail.com>
2019-01-30 21:37:45 +0800

    Update site-map.vue

--- pages/site-map.vue
+++ pages/site-map.vue
@@ -12,37 +12,37 @@ export default {
     <li><a href="index.html">Home</a></li>
     <li>About Us
       <ul>
-        <li><a href="companyprofile-crew.html">Company Profile & Crew</a></li>
-        <li><a href="fleet-information.html">Fleet Information</a></li>
-        <li><a href="ambulance-equipment-list.html">Ambulance Equipment List</a></li>
-        <li><a href="what-news.html">What's New</a></li>
+        <li><a href="/companyprofile-crew">Company Profile & Crew</a></li>
+        <li><a href="/fleet-information">Fleet Information</a></li>
+        <li><a href="/ambulance-equipment-list">Ambulance Equipment List</a></li>
+        <li><a href="/what-new">What's New</a></li>
 
       </ul>
     </li>
     <li>Transport
       <ul>
-        <li><a href="transport.html">Local Transport</a></li>
-        <li><a href="transport.html#critical-ill">Transport of Critically Ill</a></li>
-        <li><a href="transport.html#overseas-transport">Overseas Transport</a></li>
+        <li><a href="/transport">Local Transport</a></li>
+        <li><a href="/transport#critical-ill">Transport of Critically Ill</a></li>
+        <li><a href="/transport#overseas-transport">Overseas Transport</a></li>
 
       </ul>
     </li>
     <li>Event Standby
       <ul>
-        <li><a href="event-standby.html">Information</a></li>
+        <li><a href="/event-standby">Information</a></li>
 
-        <li><a href="gallery.html">Gallery</a></li>
+        <li><a href="/gallery">Gallery</a></li>
 
       </ul>
     </li>
     <li>Sales & Rental
       <ul>
-        <li><a href="sales-purchase.html">Rental of Oxygen Cylinders</a></li>
-        <li><a href="sales-purchase.html#purchaseAED">Sales of AED</a></li>
+        <li><a href="/sales-purchase">Rental of Oxygen Cylinders</a></li>
+        <li><a href="/sales-purchase#purchaseAED">Sales of AED</a></li>
 
       </ul>
     </li>
-    <li><a href="contact-us.html">Contact Us</a></li>
+    <li><a href="/contact-us">Contact Us</a></li>
   </ul>
   <MapComponent />
   <CallBanner />
