e0e06053d9c1361c506e6787713d78fa248cb477
carlo-activamedia <35211529+carlo-activamedia@users.noreply.github.com>
2023-12-13 15:42:12 +0800

    content and price update (#39)
    * bulk
    
    - applied jsonLD
    - applied internal links
    - applied alt texts
    
    * applied hreflang tags
    
    * bulk update
    
    - removed unnecessary commas
    - whats-new content update
    - fix hreflang in contact us page
    
    * bulk update
    
    - iso logo
    - new event
    - add number
    - content update
    - replace images
    - price list update
    
    * fix content and gallery page
    
    * meta title update
    
    * Update nuxt.config.js
    
    * bulk update
    
    - update banner
    - added latest events photos
    
    * change the Fax to Mobile :  (65) 8784 2484
    
    * content and price update
    
    ---------
    
    Co-authored-by: Gian Carlo Saquilayan <giancarlosaquilayan@gmail.com>

--- pages/sales-purchase.vue
+++ pages/sales-purchase.vue
@@ -35,124 +35,189 @@ export default {
 
         <p>
           AMS offers rental of oxygen cylinders for home care. Our oxygen cylinder is made of aluminium alloy and comes with an attached flow regulator.
-          It has a carrying handle for easy handling; it does not rust therefore it is not possible for the patient to inhale rust particles. Our cylinders come in the following sizes according to the volume of liquid oxygen within: 2.9L, 4.6L and 10.5L.
+          It has a carrying handle for easy handling; it does not rust therefore it is not possible for the patient to inhale rust particles. Our cylinders come in the following sizes according to the volume of liquid oxygen within: 2.9L, 3.4L, 4.6L and 10.5L.
           <br clear="both">
         </p>
 
+        <h3>Rent and Sales of Oxygen Cylinders & Regulartors</h3>
         <table-wrap>
           <table style="width:100%; min-width: 600px">
+            <thead>
+              <tr>
+                <td rowspan="2">
+                  <strong>
+                    RENTAL<br>
+                    (Cylinder + regular)
+                  </strong>
+                </td>
+                <td rowspan="2">
+                  RENTAL FEE<br>
+                  (max 1 week)
+                </td>
+                <td rowspan="2">
+                  DEPOSIT
+                </td>
+                <td rowspan="2">
+                  TROLLEY
+                </td>
+                <td colspan="2" style="text-align: center;">
+                  DELIVERY CHARGE
+                </td>
+                <td rowspan="2" style="text-align: center;">
+                  SURCHARGE PER WEEK
+                </td>
+              </tr>
+              <tr>
+                <td style="text-align: center;">1 way</td>
+                <td style="text-align: center;">2 ways</td>
+              </tr>
+            </thead>
             <tbody>
               <tr>
-                <td
-                  rowspan="2"
-                  style="text-align:center;"
-                >
-                  <strong><i>Cylinder Size <br>
-                    (Liquid<br>Volume<br> in Litres)
-                  </i></strong>
-                </td>
-                <td
-                  rowspan="2"
-                  style="text-align:center;"
-                >
-                  <strong>Pressure when full<br>(In Atm)</strong>
-                </td>
-                <td
-                  rowspan="2"
-                  style="text-align:center;"
-                >
-                  <strong>Volume of Gaseous O2 <br>(in Litres)</strong>
-                </td>
-                <td
-                  colspan="3"
-                  style="text-align:center;"
-                >
-                  <strong><i>Approximate</i> Duration based on Flow <br>(Litres/minute)</strong>
-                </td>
+                <td>2.9L</td>
+                <td style="text-align: center;">$90</td>
+                <td style="text-align: center;">$200</td>
+                <td style="text-align: center;">-</td>
+                <td style="text-align: center;">$50</td>
+                <td style="text-align: center;">$100</td>
+                <td style="text-align: center;">$30</td>
               </tr>
               <tr>
-                <td style="text-align:center;">
-                  <strong>2</strong>
-                </td>
-                <td style="text-align:center;">
-                  <strong>5</strong>
-                </td>
-                <td style="text-align:center;">
-                  <strong>10</strong>
-                </td>
+                <td>3.4L w bag</td>
+                <td style="text-align: center;">$100</td>
+                <td style="text-align: center;">$200</td>
+                <td style="text-align: center;">-</td>
+                <td style="text-align: center;">$50</td>
+                <td style="text-align: center;">$100</td>
+                <td style="text-align: center;">$30</td>
               </tr>
               <tr>
-                <td style="text-align:center;">
-                  2.9
-                </td>
-                <td style="text-align:center;">
-                  134
-                </td>
-                <td style="text-align:center;">
-                  388.6
-                </td>
-                <td style="text-align:center;">
-                  1 hr(s) 37 min(s)
-                </td>
-                <td style="text-align:center;">
-                  0 hr(s) 38 min(s)
-                </td>
-                <td style="text-align:center;">
-                  0 hr(s) 19 min(s)
-                </td>
+                <td>4.6L</td>
+                <td style="text-align: center;">$110</td>
+                <td style="text-align: center;">$200</td>
+                <td style="text-align: center;">-</td>
+                <td style="text-align: center;">$50</td>
+                <td style="text-align: center;">$100</td>
+                <td style="text-align: center;">$30</td>
               </tr>
               <tr>
-                <td style="text-align:center;">
-                  4.6
-                </td>
-                <td style="text-align:center;">
-                  134
-                </td>
-                <td style="text-align:center;">
-                  616.4
-                </td>
-                <td style="text-align:center;">
-                  2 hr(s) 34 min(s)
-                </td>
-                <td style="text-align:center;">
-                  1 hr(s) 01 min(s)
-                </td>
-                <td style="text-align:center;">
-                  0 hr(s) 30 min(s)
-                </td>
-              </tr>
-              <tr>
-                <td style="text-align:center;">
-                  10
-                </td>
-                <td style="text-align:center;">
-                  134
-                </td>
-                <td style="text-align:center;">
-                  1340
-                </td>
-                <td style="text-align:center;">
-                  5 hr(s) 35 min(s)
-                </td>
-                <td style="text-align:center;">
-                  2 hr(s) 14 min(s)
-                </td>
-                <td style="text-align:center;">
-                  1 hr(s) 07 min(s)
-                </td>
+                <td>10.5L or 10L</td>
+                <td style="text-align: center;">$120</td>
+                <td style="text-align: center;">$300</td>
+                <td style="text-align: center;">-</td>
+                <td style="text-align: center;">$50</td>
+                <td style="text-align: center;">$100</td>
+                <td style="text-align: center;">$50</td>
               </tr>
             </tbody>
           </table>
         </table-wrap>
+        
+        <ol>
+          <li>Encourage customers to deal with office for Rent or Sales of Oxygen Cylinders and self collection if possible.</li>
+          <li>Each rental period is 1 week maximum, need to return to office whether utilised or not in or before 1 week time.</li>
+          <li>To extend the rental after 1 week will be charged same rental fee for another 1 week.</li>
+          <li>Deposit will be refundable if the oxygen Is returned in good condition.</li>
+          <li>No re-fill cost, rental of another oxygen cylinder is charged according to the rental fee.</li>
+          <li>Additional cost for Rental with trolley</li>
+          <li>Collection of cylinder will be charged for another 1 way.</li>
+          <li>Rental fee (with gst) to issue Green Receipt.</li>
+          <li>The deposit (no gst), will be issued in separate Green Receipt.</li>
+        </ol>
+        <br>
+        
+        <table-wrap>
+          <table style="width:100%; min-width: 600px">
+            <thead>
+              <tr>
+                <td rowspan="2" style="text-align: center;">
+                  <strong>
+                    SALES
+                  </strong>
+                </td>
+                <td rowspan="2" style="text-align: center;">
+                  CYLINDER
+                </td>
+                <td rowspan="2" style="text-align: center;">
+                  REGULATOR
+                </td>
+                <td rowspan="2" style="text-align: center;">
+                  1 SET
+                </td>
+                <td rowspan="2" style="text-align: center;">
+                  REFILL<br>
+                  per cylinder
+                </td>
+                <td rowspan="2" style="text-align: center;">
+                  1 to 1 exchange <br>total top up<br>
+                  <strong>2 cylinders</strong>
+                </td>
+                <td colspan="2" style="text-align: center;">
+                  DELIVERY CHARGE
+                </td>
+              </tr>
+              <tr>
+                <td style="text-align: center;">1 way</td>
+                <td style="text-align: center;">2 ways</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>2.9L w bag</td>
+                <td style="text-align: center;">$320 + $100</td>
+                <td style="text-align: center;">$130</td>
+                <td style="text-align: center;">$550</td>
+                <td style="text-align: center;">$50</td>
+                <td style="text-align: center;">$100</td>
+                <td style="text-align: center;">$50</td>
+                <td style="text-align: center;">$100</td>
+              </tr>
+              <tr>
+                <td>3.4L w bag</td>
+                <td style="text-align: center;">$350 + $100</td>
+                <td style="text-align: center;">$130</td>
+                <td style="text-align: center;">$580</td>
+                <td style="text-align: center;">$60</td>
+                <td style="text-align: center;">$120</td>
+                <td style="text-align: center;">$50</td>
+                <td style="text-align: center;">$100</td>
+              </tr>
+              <tr>
+                <td>4.6L</td>
+                <td style="text-align: center;">$420</td>
+                <td style="text-align: center;">$130</td>
+                <td style="text-align: center;">$550</td>
+                <td style="text-align: center;">$70</td>
+                <td style="text-align: center;">$140</td>
+                <td style="text-align: center;">$50</td>
+                <td style="text-align: center;">$100</td>
+              </tr>
+              <tr>
+                <td>10.5L ir 10L</td>
+                <td style="text-align: center;">$520</td>
+                <td style="text-align: center;">$130</td>
+                <td style="text-align: center;">$650</td>
+                <td style="text-align: center;">$80</td>
+                <td style="text-align: center;">$160</td>
+                <td style="text-align: center;">$50</td>
+                <td style="text-align: center;">$100</td>
+              </tr>
+            </tbody>
+          </table>
+        </table-wrap>
+        
+        <ol>
+          <li>For Refill, will take at least 5 days for collection.</li>
+          <li>If customer needs to get cylinder Immediately, would suggest to exchange 1 to 1 and pay for 2 cylinders top up.</li>
+          <li>Customer will collect back their cylinder when it's filled up and return our cylinder.  </li>
+        </ol>
+        <h3>ALL QUOTES ARE SUBJECT TO GST</h3><br>
+
         <h3>Details to Provide When Calling Us</h3>
         <p>
           To help us advise you better, please provide us the answers to the following questions when making the rental:<br> - What is the medical condition that the patient suffering from? <br> - What oxygen flow rate does his doctor prescribe?
         </p>
-        <h3>Pricing Schedule</h3>
-        <p>
-          The rental fee of each cylinder varies from <strong>$70 - $100</strong> depending on the size of the cylinder. There will be no charge if clients pick up the oxygen cylinder from our warehouse; alternatively a delivery charge of $50 (office hours) or $80 (non-office hours) will be
-          imposed.
-        </p>
+        
         <h3>Replacement of depleted oxygen cylinders</h3>
         <p>
           You can contact us 24/7 to replace your depleted oxygen cylinders with full oxygen cylinders.
