177763def9d6674672e34d37d05690c5eaa463a4
dssq <danielssq89@gmail.com>
2024-05-15 23:06:36 +0800

    missing files

---
+++ components/PageHeaderLink.vue
@@ -0,0 +1,9 @@
+<template>
+  <NuxtLink custom v-bind="$attrs" v-slot="{ isActive, href, navigate }">
+    <li :class="{ active: !!isActive }">
+      <a :href="href" @click="navigate">
+        <slot />
+      </a>
+    </li>
+  </NuxtLink>
+</template>
